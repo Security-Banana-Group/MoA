684fdccf2cff827294dbe07b4c0ff1fff99168f3
Daniel H <dan303h@gmail.com>
2018-02-21 12:20:49 -0500

    add main module add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import sys
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup , find_packages
+
+
+I
+
+setup(name='MoA',
+      version='0.1',
+      description='Tools for testing a network',
+      urls='https://github.com/Security-Banana-Group/MoA',
+      author='EVT RIT',
+      author_email='peaches@RIT.EDU',
+      packages=find_packages(),
+      scripts= ['bin/cansetup'],
+      license='MIT',
+      include_package_data=False,
+      package_data={ '': ['*.txt', '*.rst']},
+      zip_safe=False,
+      entry_points ={
+      'console_scripts': [
+                  'gateway = gateway.exec:main_func_gateway', #when I determine the main method of our tools
+              ],
+      })
+
+
+BIN_PATH = '/usr/local/bin'
