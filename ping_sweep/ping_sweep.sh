6d394cacba6c67946ae7d3690e27e499d666dff1
landyy <rmb2555@rit.edu>
2018-03-04 19:26:03 +0000

    Finished ping_sweep

--- ping_sweep/ping_sweep.sh
+++ ping_sweep/ping_sweep.sh
@@ -10,6 +10,16 @@ if [[ "$#" -ne 1 ]]; then
         exit
 fi
 
+#check to see if ipcalc is installed because thats important!
+
+if [[ ! -x $(command -v ipcalc) ]]; then
+	
+	echo "The tool ipcalc is required for this script"
+	echo "Please Install ipcalc with your local package manager"
+	exit
+
+fi
+
 if [[ $1 = *"-"* ]]; then
 	seqone=$(echo $1 | sed 's/-/./' |awk -F '.' '{print $1" "$5}')
 	seqtwo=$(echo $1 | sed 's/-/./' |awk -F '.' '{print $2" "$6}')
@@ -28,10 +38,32 @@ if [[ $1 = *"-"* ]]; then
 	done
 
 elif [[ $1 = *"/"*  ]]; then
-	echo "CIDR notation"
+	low=$(ipcalc -n $1 | grep HostMin | cut -d ' ' -f 4)
+	high=$(ipcalc -n $1 | grep HostMax | cut -d ' ' -f 4)
+
+	range=$(echo $low"-"$high)
+
+	seqone=$(echo $range | sed 's/-/./' |awk -F '.' '{print $1" "$5}')
+        seqtwo=$(echo $range | sed 's/-/./' |awk -F '.' '{print $2" "$6}')
+        seqthree=$(echo $range | sed 's/-/./' |awk -F '.' '{print $3" "$7}')
+        seqfour=$(echo $range | sed 's/-/./' |awk -F '.' '{print $4" "$8}')
+
+        for i in `seq $seqone`; do
+                for j in `seq $seqtwo`; do
+                        for k in `seq $seqthree`; do
+                                for l in `seq $seqfour`; do
+                                        ipaddr="$i.$j.$k.$l"
+                                        ping -c 1 $ipaddr | tr '\n' ' ' | awk '/1 received/ {print "Host "$2" is alive" }'
+
+				done
+			done
+		done
+	done
 
 else
-	echo "Did not hit either"
+
+
+	echo "Invalid Format for IP Range"
 	exit
 fi
 
