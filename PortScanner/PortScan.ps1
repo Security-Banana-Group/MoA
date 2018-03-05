d19b42fd4e2d140a2509f04e4b2cdcd6bf7a0d17
BillK7890 <truebluedrag@yahoo.com>
2018-03-05 05:52:58 -0500

    PortScan.ps1
    Renamed function to PortScan. added ability to accept the cidr or range input. added testing for port. fixed output to display IP addresses, Open Ports, and Closed Ports. added ability to have the results exported

--- PortScanner/PortScan.ps1
+++ PortScanner/PortScan.ps1
@@ -14,7 +14,7 @@
     the ports, whether they are open or closed per address.
 
     .EXAMPLE
-    .\PortScan.ps1 
+    .\PortScan.ps1 192.168.1.1-192.168.1.5 20,80
 
 
 #>
@@ -79,10 +79,11 @@ if ($ip -match '-'){
         [Array]::Reverse($test)
         $test = $test -join '.'
 
-        $object = New-Object -TypeName PSObject
-        $object | Add-Member -Name 'IP Address' -MemberType Noteproperty -Value $test
+        #$object = New-Object -TypeName PSObject
+        #$object | Add-Member -Name 'IP Address' -MemberType Noteproperty -Value $test
 
-        $array += $object
+        #$array += $object
+        $array +=$test
     }
 
     
@@ -90,16 +91,48 @@ if ($ip -match '-'){
     
 }Elseif($ip -match '/'){
     $range = $ip.split("/")
-    $startip = [System.Net.IPAddress]::Parse($range[0])
+    
+    $s1 = ($range[0].Split(' ') -join '.')
     $cidr = $range[1]
+
+    $begin = ([System.Net.IPAddress]$s1).GetAddressBytes()
+    [Array]::Reverse($begin)
+    $begin = ([Net.IPAddress]($begin -join '.')).Address
+    
+    [int64]$int = [convert]::ToInt64(("1"*$cidr+"0"*(32-$cidr)),2)
+    $netmask = (([math]::truncate($int/16777216)).tostring()+"."+([math]::truncate(($int%16777216)/65536)).tostring()+"."+([math]::truncate(($int%65536)/256)).tostring()+"."+([math]::truncate($int%256)).tostring() )
+
+    $cause = ([System.Net.IPAddress]$int).GetAddressBytes()
+    [Array]::Reverse($cause)
+    $cause = $cause -join '.'
+        
+        
+    $intend = (([system.net.ipaddress]::parse("255.255.255.255").address -bxor $cause.address -bor $netmask.address)) 
+    $endmask = (([math]::truncate($intend/16777216)).tostring()+"."+([math]::truncate(($intend%16777216)/65536)).tostring()+"."+([math]::truncate(($intend%65536)/256)).tostring()+"."+([math]::truncate($intend%256)).tostring() )
+    
+     
+    
+    #$networkaddr = new-object net.ipaddress ($maskaddr.address -band $begin.address)
+
+    $array = @()
+
+    for ($x=$int; $x -le $intend; $x++) {
+        
+        $x3 = ($x -band $begin)
+        
+        $test = ([System.Net.IPAddress]$x3).GetAddressBytes()
+        [Array]::Reverse($test)
+        $test = $test -join '.'
+        #Write-Output $test
+        $array +=$test
+        }
     }
 
 
 #Check and separate the input for the ports
  $PortsArray = @()
  foreach($b in $ports){
-    Write-Output $b 
-    
+       
     #Checks list of ports
     if($b -lt 1 -XOR $b -gt 65535){
         return {Invalid list of ports}
@@ -129,30 +162,41 @@ if ($ip -match '-'){
 
 ####################################$PortsArray
 
+$results = @()
+$open = @()
+$closed = @()
 
-foreach($test in $addresses){
+$x1=0
+foreach($address in $array){
     
+    foreach($cPort in $PortsArray){
+        $x1 +=1
+        Write-Progress “Scanning IP ”$address':'$cPort -PercentComplete (($x1/$array.Count))
+
+        $object = New-Object -TypeName PSObject
+        $object | Add-Member -Name 'IP' -MemberType Noteproperty -Value $address
+
+        
+        $socket = New-Object System.Net.Sockets.TcpClient($cIP, $cPort)
     
-
-    $object = New-Object -TypeName PSObject
-    $object | Add-Member -Name 'IP Address' -MemberType Noteproperty -Value $ip
-    $object | Add-Member -Name 'Open Ports' -MemberType Noteproperty -Value $open
-    $object | Add-Member -Name 'Closed Ports' -MemberType Noteproperty -Value $closed
-
-    $array += $object
-    }
-
-try{
-    $socket = New-Object System.Net.Sockets.TcpClient($cIP, $cPort)
-    
-    if($socket.Connected){
-        "$cIP port $cPort open"
-        $socket.Close()
-    }else{
-        "$cIP port $cPort closed"
+        if($socket.Connected){
+            if(-Not $open.Contains($cPort)){
+                $open += $cPort               
+                    }
+            $socket.Close()
+        }else{
+            if(-Not $closed.Contains($cPort)){
+                $closed += $cPort               
+                }
+            }
         }
-
+        $object | Add-Member -Name 'OP' -MemberType Noteproperty -Value $open
+        $object | Add-Member -Name 'CP' -MemberType Noteproperty -Value $closed
+        $results += $object
     }
-catch{$status = "Closed"}
-
+    
+    #$lol
+    #Select $results.Members type 
+    #$results | Select-Object -Property 'IP Address'={$_.IP},'Open Ports'={$_.OP},'Closed Ports'={$_.CP}
+    $results | Select @{Name="IP Address";Expression={$_.IP}}, @{Name="Open Ports";Expression={$_.OP}}, @{Name="Closed Ports";Expression={$_.CP}}  #Export-Csv –Path P:\PortScan.csv
 }
